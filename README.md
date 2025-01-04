9fb0f0b163612af585e0b15300a2574f14082449
Widi MARTIAL <widi.martial@gmail.com>
2025-01-04 16:27:26 +0100

    improve accessibility

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ This is the starter template for the Next.js App Router Course. It contains the 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
 
 ## Progress practice
-- Chapter 14
+- Chapter 15
 
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   experimental: {
-    ppr: 'incremental'
-  }
+    ppr: "incremental",
+  },
 };
 
 export default nextConfig;
