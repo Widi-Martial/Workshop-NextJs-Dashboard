60b439480d770662f2594b37b4cf33c85acd064b
Widi MARTIAL <widi.martial@gmail.com>
2025-01-06 15:16:24 +0100

    config authenfication

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
   errors?: {
@@ -66,7 +68,6 @@ export async function createInvoice(prevState: State, formData: FormData){
     redirect('/dashboard/invoices'); 
 }
 
-
 export async function updateInvoice(id: string,prevState: State, formData: FormData) {
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
@@ -99,7 +100,7 @@ export async function updateInvoice(id: string,prevState: State, formData: FormD
   
   }
 
-  export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string) {
     throw new Error('Failed to Delete Invoice')
 
     try {
@@ -109,4 +110,23 @@ export async function updateInvoice(id: string,prevState: State, formData: FormD
     } catch (error) {
       return { message: 'Database Error: Failed to Delete Invoice.' };
     } 
+  }
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
